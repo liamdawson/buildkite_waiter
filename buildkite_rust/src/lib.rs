35c6d8283bd84c2b4dc0e6674bfb4175b35140ee
Liam Dawson <liam@ldaws.com>
2020-09-04 11:52:54 +1000

    Collapse workspace

--- buildkite_rust/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod client;
-mod models;
-mod response;
-
-pub mod api;
-
-pub use client::*;
-pub use models::*;
-pub use response::*;
--- buildkite_rust/src/models/mod.rs
+++
@@ -1,9 +0,0 @@
-// Type mappings for semantic purposes
-
-mod build;
-mod job;
-mod user;
-
-pub use build::*;
-pub use job::*;
-pub use user::*;
